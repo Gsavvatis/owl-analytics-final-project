3e7f33869716e25f665b32bb37ef5f8669970a45
George Savvatis <gsavvatis@hotmail.com>
2026-06-28 13:27:12 +0300

    Fetch 1000 records for 10 symbols serially from Binance API (10,000 records total)

--- scripts/part_1.py
+++ scripts/part_1.py
@@ -5,32 +5,46 @@ import requests
 
 
 url = "https://data-api.binance.vision/api/v3/klines"
-params = {
-    "symbol": "BTCUSDT",
-    "interval": "1h",
-    "limit": 1000,
-}
 
-response = requests.get(url, params=params, timeout=30)
-response.raise_for_status()
-
-records = response.json()
+symbols = ["BTCUSDT",
+            "ETHUSDT",
+            "BNBUSDT",
+            "SOLUSDT",
+            "XRPUSDT",
+            "ADAUSDT",
+            "DOGEUSDT",
+            "AVAXUSDT",
+            "LINKUSDT",
+            "DOTUSDT"
+            ]
 rows = []
-for record in records:   
-    row = {
-        "symbol": "BTCUSDT",
+for symbol in symbols:
+    params = {
+        "symbol": symbol,
         "interval": "1h",
-        "open_time": record[0],
-        "open": record[1],
-        "high": record[2],
-        "low": record[3],
-        "close": record[4],
-        "volume": record[5],
-        "close_time": record[6],
-        "quote_volume": record[7],
-        "trade_count": record[8],
+        "limit": 1000,
     }
-    rows.append(row)
+
+    response = requests.get(url, params=params, timeout=30)
+    response.raise_for_status()
+
+    records = response.json()
+    
+    for record in records:   
+        row = {
+            "symbol": symbol,
+            "interval": "1h",
+            "open_time": record[0],
+            "open": record[1],
+            "high": record[2],
+            "low": record[3],
+            "close": record[4],
+            "volume": record[5],
+            "close_time": record[6],
+            "quote_volume": record[7],
+            "trade_count": record[8],
+        }
+        rows.append(row)
 
 output_path = Path("data/clean/records.csv")
 output_path.parent.mkdir(parents=True, exist_ok=True)
